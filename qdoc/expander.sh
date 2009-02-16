7081f2881ce25e3d7ee8886c71b7d95709a5792a
weaves <weaves@b387b25f-291c-0410-8dc7-e51caacd5b47>
2009-02-16 19:47:01 +0000

    -

--- qdoc/expander.sh
+++ qdoc/expander.sh
@@ -9,6 +9,7 @@ test $# -ge 1 || exit -1
 test -f $1 || exit -1
 
 tfile=t.$$
+tfile1=t1.$$
 
 (
 echo "cat <<EOF"
@@ -16,6 +17,14 @@ cat $1
 echo ""
 echo "EOF" ) > $tfile
 
-$SHELL $tfile
-rm -f $tfile
+$SHELL $tfile > $tfile1
+
+cat $tfile1 | egrep '[A-Z]+_OUTPUT[ ]*=[ ]*' | awk -F= '{ print $2 }' | while read i
+do
+ test -d "$i" || mkdir -p "$i"
+done
+
+cat $tfile1
+
+rm -f $tfile $tfile1
 
