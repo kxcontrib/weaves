5d5f89988ffe2826417b42496165b1edfd827c03
weaves <weaves@b387b25f-291c-0410-8dc7-e51caacd5b47>
2009-08-09 17:51:12 +0000

    -

--- contrib/weaves/q4r/R/q4r.R
+++ contrib/weaves/q4r/R/q4r.R
@@ -1,12 +1,30 @@
 ##' @brief Q/kdb for R 
 ##' @file q4r.R
 
-open.q <- function(host="localhost", port=1444, user=NULL) {
-	if(is.null(host) || is.na(host)) host <- "localhost"
-	if(is.null(port) || is.na(port)) port <- 1444
+open.q <- function(host="localhost", port=1444, user=NULL, hsym = NULL,
+                   verbose = getOption("verbose")) {
+  if(!is.null(hsym))
+    host <- (unlist(strsplit(hsym, ":")))[[2]]
 
-         parameters <- list(host, as.integer(port), user)
-         .Call("kx_r_open_connection", parameters)
+  if(!is.null(hsym))
+    port <- as.integer((unlist(strsplit(hsym, ":")))[[3]])
+
+  if(is.null(host) || is.na(host)) host <- "localhost"
+  if(is.null(port) || is.na(port)) port <- 1444
+
+  parameters <- list(host, as.integer(port), user)
+  if(verbose) print(parameters)
+  .Call("kx_r_open_connection", parameters)
+}
+
+open1.q <- function(host="localhost", port=1444, user=NULL, hsym = NULL) {
+  if(!is.null(hsym))
+    
+    if(is.null(host) || is.na(host)) host <- "localhost"
+  if(is.null(port) || is.na(port)) port <- 1444
+
+  parameters <- list(host, as.integer(port), user)
+  .Call("kx_r_open_connection", parameters)
 }
 
 close.q <- function(connection) {
