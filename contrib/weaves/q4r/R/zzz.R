4aea3fad1188e2ee1e3f4ca17fb66f5e2516f770
weaves <weaves@b387b25f-291c-0410-8dc7-e51caacd5b47>
2009-08-09 09:40:59 +0000

    Builds and installs on l32

--- contrib/weaves/q4r/R/zzz.R
+++ contrib/weaves/q4r/R/zzz.R
@@ -1,15 +1,25 @@
+# Manual version
 load.q <- function() {
-  dyn.load(paste(.libPaths()[[1]], "qserver.so", sep="/"))
+  lib.loc = paste(.libPaths()[[1]], "q4r", "libs", "q4r.so", sep="/")
+  dyn.load(lib.loc)
 }
 
 .First.lib <- function(lib, pkg) {
-  library.dynam("qserver", pkg, lib, lib.loc = .libPaths()[[1]] )
+  # not called because this package has a namespace
 }
 
 .onLoad <- function(libname, pkgname) {
   cat("This is a pre-release. The interface might change...\n")
+  # cat(paste(libname, pkgname))
+  # cat("This is a pre-release. The interface might change...\n")
+  library.dynam("q4r", pkgname, lib.loc = libname, verbose = TRUE )
+  # load.q()
+}
+
+.onAttach <- function(libname, pkgname) {
+  # cat("This is a pre-release. The interface might change...\n")
   # library.dynam("qserver", pkg, lib, lib.loc = .libPaths()[[1]] )
-  load.q()
+  # load.q()
 }
 
 
