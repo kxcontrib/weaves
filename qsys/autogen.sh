9250f001c56edb8d3debd26cdbee6ae310948513
weaves <weaves@b387b25f-291c-0410-8dc7-e51caacd5b47>
2009-11-07 20:20:59 +0000

    Using git locally

--- qsys/autogen.sh
+++ qsys/autogen.sh
@@ -3,11 +3,33 @@
 # weaves
 # This script is an example of how to run configure and build
 
+# The build uses SVN to get externals: see INSTALL
+
 cd smet
 ./autogen.sh
 cd -
 
+# Sometimes, I use git and that doesn't support externals.
+if [ -d ../.git ]
+then
+ test -d lib || mkdir lib
+ ( 
+ cd lib 
+ find . -maxdepth 1 -type l -exec rm -f {} \;
+ cp -sr ../../../../kx/kdb+/l32/* .
+ )
+ 
+ test -d qdoc || ln -s ../qdoc .
+ ( 
+ cd qdoc/cfg
+ ln -sf ~-/dev.doxy.in . 
+ )
+fi
+
 cp lib2/Makefile.am lib
+
+# autoreconf doesn't add missing files
+aclocal && automake --add-missing --copy
 autoreconf --force --install
 ./configure --prefix=$HOME --with-qhomedir=$HOME/src/q --with-string-metrics --with-qtrdrhost=$HOSTNAME --with-qtrdrport=15001 --disable-dependency-tracking
 make
